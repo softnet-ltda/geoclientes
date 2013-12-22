fe475144db9852859e8e8f886af5856f37b421c6
Francisco Arevalo <farevalod@gmail.com>
2013-12-22 19:12:12 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoclientes::Application.config.secret_token = '00d24f259c693bbf73f2fbd226e1948e9b01eeb002940fc289005e306a800a12c96dac0827e663a462ec799676068cee4fc8e75e53510e3a13592f92f6a4d897'
