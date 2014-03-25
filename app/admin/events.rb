f423bfe6a06773292aff28c1cefe2731af06bcc6
Paul Nicolet <paulyarur@gmail.com>
2014-03-25 18:55:29 -0300

    Events time picker

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,13 +1,14 @@
 ActiveAdmin.register Event do
-  index do                            
-    column :date        
+  index :title => "Eventos" do
+    column :date, :label => "Fecha"
     column :status
 	column :type, :label => "Tipo"
-    default_actions                   
-  end                                 
+    default_actions
+  end
   form :html => { :enctype => "multipart/form-data" } do |f|
 	  f.inputs "Details" do
 		  f.input :date, :as => :datepicker, :label => "Fecha"
+		  f.input :date, :as => :time_picker, :label => "Hora"
 		  f.input :status
 		  f.input :type, :label => "Tipo"
 	  end
