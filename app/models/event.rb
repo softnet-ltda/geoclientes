f693c3a8e99f7ebb34804f67fd8dc024b6d10f6c
Paul Nicolet <paulyarur@gmail.com>
2014-03-27 21:45:01 -0300

    attribute accessible model event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :date, :status, :type
+  attr_accessible :date, :status, :type, :user_id, :contact_id
   belongs_to :user
   belongs_to :contact
 end
