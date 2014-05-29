24e90df7509301e276cdfd04efb6cc736ef0ae4c
Francisco Arevalo <farevalod@gmail.com>
2014-05-29 16:46:53 -0400

    Roles para usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140509193936) do
+ActiveRecord::Schema.define(:version => 20140529204151) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(:version => 20140509193936) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.integer  "supervisor_id"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
