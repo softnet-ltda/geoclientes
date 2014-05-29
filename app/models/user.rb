70ef4460e1fd3b14c92545d496751d9f8b45abaf
Francisco Arevalo <farevalod@gmail.com>
2014-05-29 16:58:55 -0400

    Supervisor id relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :name
+  attr_accessible :name, :role, :supervisor_id
   has_many :events
   has_many :customers
+  belongs_to :user, foreign_key: "supervisor_id"
+  has_many :users, foreign_key: "supervisor_id"
 end
